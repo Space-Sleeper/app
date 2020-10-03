2e11ffc5725f20e8467635d612cfd5be1080dfcf
seito <seito1201@gmail.com>
2020-10-03 20:45:31 +0900

    [fix] delete unuse value

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Switch, useHistory } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import { PageRoute } from "./route";
 import HomePage from "./pages/Home";
 import SleepPage from "./pages/Sleep";
