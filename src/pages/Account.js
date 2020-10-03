61b188e1c54d879b272d8a66e964c44049da929c
naoki <n.globe.us@gmail.com>
2020-10-03 23:04:06 +0900

    [add]css of account page

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -1,27 +1,42 @@
 import React from "react";
-import { TextField } from 'ui-neumorphism'
+import { TextField, RadioGroup, Radio } from 'ui-neumorphism'
 import 'ui-neumorphism/dist/index.css'
-import './Account.css'
+import Styles from './Account.module.css'
 
 export default function AccountPage() {
+
+  const warningNegativeNum = [
+    (v) => v > 0 || 'Cant be negative number',
+  ];
+
   return (
-    <div className={"container"}>
-      <h1>Body Information</h1>
-      <h2>Gender</h2>
-      <h2>Age</h2>
+    <div className={Styles.container}>
+      <h1 className={Styles.h1}>Body <br></br>Information</h1>
+      <h2 className={Styles.h2}>Gender</h2>
+      <RadioGroup value='1' color='#596DE4' className={Styles.input} onChange={()=>{console.log(1)}}>
+        <Radio value='1' label='Female' />
+        <Radio value='2' label='Male' />
+        <Radio value='3' label='Other'/>
+      </RadioGroup>
+      <h2 className={Styles.h2}>Age</h2>
       <TextField
-        className='age'
+        className={Styles.input}
         type='number'
+        rules={warningNegativeNum}
       ></TextField>
-      <h2>Height</h2>
+      <h2 className={Styles.h2}>Height</h2>
       <TextField
-        className='height'
+        append='cm'
+        className={Styles.input}
         type='number'
+        rules={warningNegativeNum}
       ></TextField>
-      <h2>Weight</h2>
+      <h2 className={Styles.h2}>Weight</h2>
       <TextField
-        className='weight'
+        append='kg'
+        className={Styles.input}
         type='number'
+        rules={warningNegativeNum}
       ></TextField>
     </div>
   );
