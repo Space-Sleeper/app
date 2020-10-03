bfc8fa82f1c846fae8f8e473b844c1f26237d39d
seito <seito1201@gmail.com>
2020-10-04 01:46:49 +0900

    [add] top page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,18 @@
 import React from "react";
 import ImageCurry from "../assets/curry.jpg";
 import Margin from "../components/margin";
+import FoodCard from "../components/foodCard";
 import { Button } from "ui-neumorphism";
+import { foodList } from "../dummy";
 
 import Styles from "./Home.module.css";
 
 export default function HomePage() {
-  console.log(ImageCurry);
+  const data = foodList;
+  data.length = 2;
 
   return (
-    <div className={Styles.contaner}>
+    <div className={Styles.container}>
       <Margin height={20} />
       <h1 className={Styles.title}>
         Next
@@ -28,7 +31,7 @@ export default function HomePage() {
             style={{
               fontSize: 20,
               padding: "0 40px",
-              "--btn-height": "60px",
+              "--btn-height": "44px",
               fontWeight: 600,
             }}
             rounded
@@ -40,6 +43,29 @@ export default function HomePage() {
       <Margin height={20} />
       <section>
         <h2 className={Styles.subtitle}>Food List</h2>
+        <Margin height={20} />
+        <div>
+          {data.map((props) => (
+            <>
+              <FoodCard {...props} key={props.id} />
+              <Margin height={20} />
+            </>
+          ))}
+        </div>
+        <div className={Styles.moreButton}>
+          <Button
+            color="var(--primary)"
+            style={{
+              fontSize: 20,
+              padding: "0 52px",
+              "--btn-height": "40px",
+              fontWeight: 600,
+            }}
+            rounded
+          >
+            More
+          </Button>
+        </div>
       </section>
     </div>
   );
